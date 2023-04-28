7881f38c6f139f0c61b0685f1f2d5ad6ae2eb8e9
zemnu <zemnuhov2405@gmail.com>
2023-04-28 17:41:12 +0300

    UpdateMarkup

--- feature-screen-markup-impl/src/main/java/com/example/feature_screen_markup_impl/MarkupFragment.kt
+++ feature-screen-markup-impl/src/main/java/com/example/feature_screen_markup_impl/MarkupFragment.kt
@@ -3,8 +3,8 @@ package com.example.feature_screen_markup_impl
 import android.annotation.SuppressLint
 import android.app.TimePickerDialog
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,28 +12,34 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.cesarferreira.tempo.beginningOfDay
+import com.cesarferreira.tempo.endOfDay
 import com.cesarferreira.tempo.toString
 import com.example.feature_screen_markup_impl.adapter.CauseAdapter
 import com.example.feature_screen_markup_impl.adapter.ResultAdapter
-import com.example.feature_screen_markup_impl.adapter.ResultAdapterModel
 import com.example.feature_screen_markup_impl.databinding.FragmentMarkupBinding
 import com.example.feature_screen_markup_impl.di.MarkupComponent
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.series.BarGraphSeries
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import com.neurotech.core_database_api.model.Cause
 import com.neurotech.core_database_api.model.ResultTenMinute
 import com.neurotech.core_database_api.model.ResultsTenMinute
+import com.neurotech.utils.StressLogger.log
 import com.neurotech.utils.TimeFormat
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Provider
 import com.example.values.R as values
 
-class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAdapterCallback, CauseAdapter.CauseCallback {
+class MarkupFragment : Fragment(R.layout.fragment_markup), ResultAdapter.ResultAdapterCallback,
+    CauseAdapter.CauseCallback {
 
     @Inject
     lateinit var factory: Provider<MarkupViewModel.Factory>
 
-    private val viewModel: MarkupViewModel by viewModels{
+    private val viewModel: MarkupViewModel by viewModels {
         factory.get()
     }
 
@@ -43,6 +49,9 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
 
     private var resultAdapter: ResultAdapter? = null
     private var causeAdapter: CauseAdapter? = null
+
+    private var barSeries = BarGraphSeries(arrayOf<DataPoint>())
+    private var tonicSeries = LineGraphSeries(arrayOf<DataPoint>())
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -54,7 +63,7 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMarkupBinding.inflate(inflater,container, false)
+        _binding = FragmentMarkupBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -63,47 +72,17 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.markupRecyclerView.layoutManager = LinearLayoutManager(context)
         resultAdapter = ResultAdapter()
         resultAdapter?.callback = this
-        binding.markupRecyclerView.adapter = resultAdapter
 
-        binding.sourceRecyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
+        binding.sourceRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         causeAdapter = CauseAdapter()
         causeAdapter?.callback = this
         binding.sourceRecyclerView.adapter = causeAdapter
 
 
-        viewModel.resultForMarkup.observe(viewLifecycleOwner){
-            if(it.list.isNotEmpty()){
-                binding.markupRecyclerView.visibility = View.VISIBLE
-                resultAdapter?.submitList(
-                    it.list.map {
-                        ResultAdapterModel(
-                            it.time,
-                            it.peakCount,
-                            it.tonicAvg,
-                            it.conditionAssessment,
-                            it.stressCause,
-                            it.keep,
-                            false,
-                            when(it.peakCount){
-                                in 0..viewModel.user.phaseNormal -> {
-                                    ContextCompat.getColor(requireContext(), values.color.green_active)
-                                }
-                                in viewModel.user.phaseNormal+1..viewModel.user.phaseNormal * 2 -> {
-                                    ContextCompat.getColor(requireContext(), values.color.yellow_active)
-                                }
-                                else  -> {
-                                    ContextCompat.getColor(requireContext(), values.color.red_active)
-                                }
-                            }
-                        )
-                    }
-                )
-            }else{
-                binding.markupRecyclerView.visibility = View.GONE
-            }
+        viewModel.results.observe(viewLifecycleOwner) {
 
         }
 
@@ -113,7 +92,7 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
             val list = resultAdapter?.currentList
             list?.forEachIndexed { index, resultAdapterModel ->
                 val time = resultAdapterModel.time.toString(TimeFormat.timePattern)
-                if(time in beginPeriod..endPeriod){
+                if (time in beginPeriod..endPeriod) {
                     list[index].isChecked = true
                 }
             }
@@ -121,7 +100,7 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
             resultAdapter?.notifyDataSetChanged()
         }
 
-        viewModel.causes.observe(viewLifecycleOwner){
+        viewModel.causes.observe(viewLifecycleOwner) {
             causeAdapter?.submitList(it.values)
         }
 
@@ -148,13 +127,21 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
     }
 
     @SuppressLint("SetTextI18n")
-    private fun setTimeChoiceListener(){
+    private fun setTimeChoiceListener() {
         binding.beginTime.setOnClickListener {
             val calendar = Calendar.getInstance()
             TimePickerDialog(
                 activity,
                 { _, hour, minute ->
-                    binding.beginTime.text = "${if (hour<10){ "0$hour" }else hour}:${if (minute<10){ "0$minute" }else minute}"
+                    binding.beginTime.text = "${
+                        if (hour < 10) {
+                            "0$hour"
+                        } else hour
+                    }:${
+                        if (minute < 10) {
+                            "0$minute"
+                        } else minute
+                    }"
                 },
                 calendar.get(Calendar.HOUR_OF_DAY),
                 calendar.get(Calendar.MINUTE),
@@ -167,7 +154,15 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
             TimePickerDialog(
                 activity,
                 { _, hour, minute ->
-                    binding.endTime.text = "${if (hour<10){ "0$hour" }else hour}:${if (minute<10){ "0$minute" }else minute}"
+                    binding.endTime.text = "${
+                        if (hour < 10) {
+                            "0$hour"
+                        } else hour
+                    }:${
+                        if (minute < 10) {
+                            "0$minute"
+                        } else minute
+                    }"
                 },
                 calendar.get(Calendar.HOUR_OF_DAY),
                 calendar.get(Calendar.MINUTE),
@@ -178,7 +173,7 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
 
     override fun isCheckedClick(position: Int, isCheck: Boolean) {
         val list = resultAdapter?.currentList
-        if(list != null && position < list.size){
+        if (list != null && position < list.size) {
             list[position]?.isChecked = isCheck
         }
         resultAdapter?.submitList(list)
@@ -188,13 +183,75 @@ class MarkupFragment: Fragment(R.layout.fragment_markup), ResultAdapter.ResultAd
     override fun clickCause(cause: Cause) {
         val list = resultAdapter?.currentList
         list?.forEachIndexed { index, resultAdapterModel ->
-            if (resultAdapterModel.isChecked){
+            if (resultAdapterModel.isChecked) {
                 list[index].isChecked = false
                 list[index].stressCause = cause.name
             }
         }
         resultAdapter?.submitList(list)
         resultAdapter?.notifyDataSetChanged()
+    }
+
+    private fun graphSettings() {
+        val minX = Date(barSeries.lowestValueX.toLong()).beginningOfDay.time.toDouble() - 500000
+        val maxX = Date(barSeries.highestValueX.toLong()).endOfDay.time.toDouble() + 500000
+        binding.markupGraph.apply {
+            removeAllSeries()
+            addSeries(tonicSeries)
+            addSeries(barSeries)
+            setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    values.color.card_background
+                )
+            )
+            viewport.isXAxisBoundsManual = true
+            viewport.isYAxisBoundsManual = true
+            viewport.setMinX(minX)
+            viewport.setMaxX(maxX)
+            viewport.maxXAxisSize = maxX - minX
+            viewport.setMinY(0.0)
+            viewport.setMaxY(tonicSeries.highestValueY + 2)
+            viewport.isScalable = true
+            viewport.isScrollable = true
+            viewport.setScalableY(false)
+            viewport.setScrollableY(false)
+            gridLabelRenderer.padding = 16
+            gridLabelRenderer.gridColor =
+                ContextCompat.getColor(requireContext(), values.color.graph_grid)
+            gridLabelRenderer.isVerticalLabelsVisible = false
+            gridLabelRenderer.numHorizontalLabels = 10
+            gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+                override fun formatLabel(value: Double, isValueX: Boolean): String {
+                    val date = Date(value.toLong())
+                    return when (date.toString("mm").toInt() % 10) {
+                        0 -> date.toString("HH:mm")
+                        else -> ""
+                    }
+                }
+            }
+        }
+        barSeries.apply {
+            barSeries.spacing = 20
+            val normal = viewModel.user.phaseNormal
+            log(normal.toString())
+            setValueDependentColor { data: DataPoint ->
+                if (data.y < normal) {
+                    return@setValueDependentColor ContextCompat.getColor(
+                        requireContext(),
+                        values.color.green_active
+                    )
+                }
+                if (normal <= data.y && data.y <= (normal * 2)) {
+                    return@setValueDependentColor ContextCompat
+                        .getColor(requireContext(), values.color.yellow_active)
+                }
+                ContextCompat.getColor(requireContext(), values.color.red_active)
+            }
+        }
+
+        tonicSeries.color = Color.BLACK
+        binding.markupGraph.invalidate()
     }
 
 
--- feature-screen-markup-impl/src/main/java/com/example/feature_screen_markup_impl/MarkupViewModel.kt
+++ feature-screen-markup-impl/src/main/java/com/example/feature_screen_markup_impl/MarkupViewModel.kt
@@ -1,15 +1,27 @@
 package com.example.feature_screen_markup_impl
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
+import com.cesarferreira.tempo.Tempo
+import com.cesarferreira.tempo.beginningOfDay
+import com.cesarferreira.tempo.endOfDay
+import com.cesarferreira.tempo.minute
+import com.cesarferreira.tempo.plus
+import com.cesarferreira.tempo.toDate
+import com.cesarferreira.tempo.toString
 import com.neurotech.core_database_api.ResultApi
 import com.neurotech.core_database_api.SettingApi
 import com.neurotech.core_database_api.UserApi
+import com.neurotech.core_database_api.model.ResultTenMinute
 import com.neurotech.core_database_api.model.ResultsTenMinute
+import com.neurotech.utils.TimeFormat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -33,17 +45,69 @@ class MarkupViewModel(
     }
 
     val causes = liveData {
-        settingApi.getCauses().collect{
+        settingApi.getCauses().collect {
             emit(it)
         }
     }
 
     val user = runBlocking { userApi.getUser() }
 
-    fun saveMarkups(results: ResultsTenMinute){
+    fun saveMarkups(results: ResultsTenMinute) {
         CoroutineScope(Dispatchers.IO).launch {
             resultApi.updateResultTenMinute(results)
         }
+    }
+
+    private var date = Tempo.now
+    private val xLabelOfDay = mutableListOf<String>().apply {
+        var time = Tempo.now.beginningOfDay
+        while (time < date.endOfDay) {
+            add(time.toString("HH:mm"))
+            time += 10.minute
+        }
+    }
+
+    private val _results = MutableLiveData<ResultsMarkup>()
+    val results: LiveData<ResultsMarkup> get() = _results
+
+
+    init {
+        viewModelScope.launch {
+            resultApi.getResultsInInterval(date.beginningOfDay, date.endOfDay)
+                .collect { resultsTenMinute ->
+                    with(resultsTenMinute.list) {
+                        val resultTimes = this.map { it.time.toString("HH:mm") }
+                        val resultMutableList = this.toMutableList()
+                        xLabelOfDay.forEach {
+                            if (it !in resultTimes) {
+                                resultMutableList.add(
+                                    ResultTenMinute(
+                                        "${date.toString(TimeFormat.dateIsoPattern)} $it:00.000".toDate(
+                                            TimeFormat.dateTimeIsoPattern
+                                        ),
+                                        0, 0, 1, null, null
+                                    )
+                                )
+                            }
+                        }
+                        _results.postValue(ResultsMarkup(resultMutableList.sortedBy { it.time }
+                            .map {
+                                ResultMarkup(
+                                    it.time,
+                                    it.peakCount,
+                                    it.tonicAvg,
+                                    it.conditionAssessment,
+                                    it.stressCause,
+                                    it.keep
+                                )
+                            }
+                        )
+                        )
+                    }
+
+                }
+        }
+
     }
 
 
