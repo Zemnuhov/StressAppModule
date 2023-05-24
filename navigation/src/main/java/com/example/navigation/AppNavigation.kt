9fb24d5e0231e16e497528f3d16e45688d31bab6
zemnu <zemnuhov2405@gmail.com>
2023-05-24 14:30:52 +0300

    bug fix

--- navigation/src/main/java/com/example/navigation/AppNavigation.kt
+++ navigation/src/main/java/com/example/navigation/AppNavigation.kt
@@ -1,25 +1,11 @@
 package com.example.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.core.view.isVisible
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.navigation.di.NavigationComponent
 import com.example.navigation_api.NavigationApi
 import com.example.navigation_api.NavigationState
-import com.example.navigation_api.ViewID
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class AppNavigation : NavigationApi {
 
